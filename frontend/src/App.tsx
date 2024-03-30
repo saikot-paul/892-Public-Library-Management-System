8264c856274aaa1febc18b01cee0f032e117e1fa
Abdullah <327abdullah.aslam@gmail.com>
2024-03-30 17:56:26 -0400

    Added routes. Login page and Home page so far

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,20 @@
 //import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import Typography from "@mui/material/Typography";
+
+function Login() {
+    return ( 
+        <Typography>This is Login</Typography>
+     );
+}
+
+export default Login;
