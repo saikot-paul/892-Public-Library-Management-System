80dfb8c8ee2ea52c4c7096d481d68212f5b4db07
Gary Lai <gary.lai@torontomu.ca>
2024-03-31 01:28:20 -0400

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
 from books.router import search_books, checkout_books, crud_books
+from users.router import recommendations, waitlist
 
 app = FastAPI()
 
 app.include_router(search_books.router)
 app.include_router(crud_books.router)
 app.include_router(checkout_books.router)
+
+app.include_router(recommendations.router)
+app.include_router(waitlist.router)
+app.include_router(borrowed.router)
