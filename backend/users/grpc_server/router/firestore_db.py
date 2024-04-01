08eebf3487a4ebc04c9f1da3d65f3f0c6234fa2b
Gary Lai <gary.lai@torontomu.ca>
2024-04-01 19:51:37 -0400

    completed user grpc server

---
+++ backend/users/grpc_server/router/firestore_db.py
@@ -0,0 +1,7 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+creds = credentials.Certificate('config.json')
+firebase_admin.initialize_app(creds)
+db = firestore.client()
