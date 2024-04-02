2af78f02ce10ac3efd8e1756f006ffba4183ce78
Saikot Paul <saikotpaul123@gmail.com>
2024-04-01 21:47:53 -0400

    fixed borrowed books endpoint

--- backend/users/router/firestore_db.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-creds = credentials.Certificate('config.json')
-firebase_admin.initialize_app(creds)
-db = firestore.client()
